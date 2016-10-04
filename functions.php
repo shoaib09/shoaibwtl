500a76b4788ed4373b9c9400f2dfc009b238ab60
shoaib09 <shoaibmustafashaikh@gmail.com>
2016-10-04 16:45:19 +0530

    first commit

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+function fix_name($name)
+{
+	$name=trim($name);
+	$name=ucfirst($name);
+	$name=addslashes($name);
+	return $name;
+}
+function fix_email($email)
+{
+	$email=trim($email);
+	$email=strtolower($email);
+	$email=addslashes($email);
+	return $email;
+}
+function fix_address($address)
+{
+	$address=trim($address);
+	$address=strtoupper($address);
+	$address=addslashes($address);
+	return $address;
+}
+
+
+?>
